31e71eecd8bf124d17999dc2bdfaea60b6b5f612
Hari <manjaka.rajaonson@gmail.com>
2022-03-30 13:29:05 +0300

    add mobile app section

---
+++ components/MobileApp.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+// Ad for mobile app
+
+const MobileApp = () => (
+	<div className="mobile-app">
+		<div className="container f-r-ce-ce">
+			<img
+				className="mobile-app__phone mg-r-20"
+				src="/images/app.png"
+				alt="Mobile app"
+			/>
+			<div className="mg-l-20">
+				<h2 className="text white">Très bientôt sur toutes les plateformes...</h2>
+				<h3 className="text fs-400 pd-b-20 pd-t-20 t">
+					L&apos;application Boba Bee !
+				</h3>
+				<div className="f-r-st-ce pd-t-20">
+					<img
+						className="p mg-r-10"
+						src="/images/google-play.png"
+						alt="Google Play Store"
+						title="Bientôt disponible sur Google Play Store !"
+					/>
+					<img
+						className="p mg-l-10"
+						src="/images/appstore.png"
+						alt="Apple store"
+						title="Bientôt disponible sur Apple Store !"
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
+);
+
+export default MobileApp;
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,23 +3,25 @@ import Heading from "components/Heading";
 import Page from "components/Page";
 import CoverHome from "components/CoverHome";
 import { PageProvider } from "hooks/usePage";
+import MobileApp from "components/MobileApp";
 
 /*
 	HomePage
 */
 
 const HomePage = () => (
-	<PageProvider defaultTab={ 0 }>
+	<PageProvider defaultTab={0}>
 		<Heading
 			title="Boba Bee, taste like honey pearl !"
 			image="/images/bobabee-logo.png"
 		>
-			Bienvenue sur le site officiel de Boba Bee, parcourez notre liste de produits.
+			Bienvenue sur le site officiel de Boba Bee, parcourez notre liste de
+			produits.
 		</Heading>
 		<Page>
 			<Fragment>
 				<CoverHome />
-				<h2>About boba bee here</h2>
+				<MobileApp />
 			</Fragment>
 		</Page>
 	</PageProvider>
